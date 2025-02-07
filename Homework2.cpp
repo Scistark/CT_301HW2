5b756e14322583390e46201719b29638f5cf6904
Eli Schneckloth <scistark@colostate.edu>
2025-02-06 20:28:09 -0700

    working on reading files

--- Homework2.cpp
+++ Homework2.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 #include <bits/stdc++.h>
+#include <sstream>
 
 class validd {
     public:
@@ -15,10 +16,26 @@ class testingopeningfile {
     void Testingopeningafile(){
     string filetext;
     ifstream textfile;
-    textfile.open("checksums.txt");
-    while(getline(textfile,filetext)){
-        cout << filetext << endl;
-    }
+    string currentline;
+    textfile.open("Test.ppm");
+    //this while loop interates through all the lines
+    while(getline(textfile,currentline)){
+        string test = currentline.substr(0,currentline.size());
+        //cout <<currentline.size() << endl;
+        //cout << currentline << "space"<<test<<"!!"<<currentline.size()<<endl;
+        if(test.c_str()[3] == '\n' || test.c_str()[3] == ' '){
+        }else{
+            cout <<test.c_str()[3]<<endl;
+        }
+        //for(int x = 0; x < currentline.size(); x++){
+            //if(test.c_str()[x] == '\n'){}else{
+            //cout << test.c_str()[x] << endl;}
+        //}
+
+        //cout << filetext << "filetext";
+
+        //cout << filetext << endl;
+        }
     }
 };
 
